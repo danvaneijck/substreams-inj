63537f73b5973f75202783ec291f4a1a83afa76d
dan <dan.ve16587@gmail.com>
2025-03-16 11:28:27 +1300

    substream and sql sink for choice swaps

---
+++ src/pb/mod.rs
@@ -0,0 +1,33 @@
+// @generated
+pub mod sf {
+    pub mod cosmos {
+        pub mod r#type {
+            // @@protoc_insertion_point(attribute:sf.cosmos.type.v2)
+            pub mod v2 {
+                include!("sf.cosmos.type.v2.rs");
+                // @@protoc_insertion_point(sf.cosmos.type.v2)
+            }
+        }
+    }
+    pub mod substreams {
+        pub mod cosmos {
+            // @@protoc_insertion_point(attribute:sf.substreams.cosmos.v1)
+            pub mod v1 {
+                include!("sf.substreams.cosmos.v1.rs");
+                // @@protoc_insertion_point(sf.substreams.cosmos.v1)
+            }
+        }
+        // @@protoc_insertion_point(attribute:sf.substreams.v1)
+        pub mod v1 {
+            include!("sf.substreams.v1.rs");
+            // @@protoc_insertion_point(sf.substreams.v1)
+        }
+    }
+}
+pub mod trades {
+    // @@protoc_insertion_point(attribute:trades.v1)
+    pub mod v1 {
+        include!("trades.v1.rs");
+        // @@protoc_insertion_point(trades.v1)
+    }
+}
